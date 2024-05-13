462ea8dbdcba6f9ee7219a6a69ce74acd9d06cba
Yohannes Takata <yohannestakata1@gmail.com>
2024-05-14 02:51:20 +0300

    Add project files.

---
+++ Palindrome Checker/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Label1.Location = New Point(263, 171)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 32)
+        Label1.TabIndex = 0
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 16F)
+        TextBox1.Location = New Point(263, 100)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(291, 50)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 16F)
+        Label2.Location = New Point(250, 22)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(304, 45)
+        Label2.TabIndex = 3
+        Label2.Text = "Palindrome Checker"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Palindrome Checker/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+    Function checkPalindrome(text As String) As Boolean
+        Dim startIndex As Integer = 0
+        Dim endIndex As Integer = text.Length - 1
+
+        While startIndex < endIndex
+            If text(startIndex) <> text(endIndex) Then
+                Return False
+            End If
+            startIndex += 1
+            endIndex -= 1
+        End While
+
+        Return True
+    End Function
+
+    Function RemoveSpecialChars(text As String) As String
+        Dim newString As String = ""
+        For Each cha As Char In text
+            If Char.IsLetterOrDigit(cha) Then
+                newString &= cha
+            End If
+        Next
+        Return newString
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim text As String = TextBox1.Text.ToLower()
+        Dim processedText = RemoveSpecialChars(text)
+
+        Dim isPalindrome As Boolean = checkPalindrome(processedText)
+
+        If isPalindrome Then
+            Label1.ForeColor = Color.LightGreen
+            Label1.BackColor = Color.DarkGreen
+            Label1.Text = "The string is a palindrome."
+        Else
+            Label1.ForeColor = Color.LightPink
+            Label1.BackColor = Color.DarkRed
+            Label1.Text = "The string is not a palindrome."
+        End If
+    End Sub
+End Class
